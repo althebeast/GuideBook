fd32547377b06d38fb3cb69d52a7d12d70629113
althebeast <alperensarisan@icloud.com>
2023-12-11 14:04:04 -0400

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Alperen Sarışan on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
